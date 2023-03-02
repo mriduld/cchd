684ca6f6af401d046155ef1de939947ae8117143
mriduld <d.mridul@gmail.com>
2023-03-03 10:08:08 +1100

    Add gitignore file

--- src/test/scala/au/org/healthdirect/nhsd/codingchallenge/SparkTestUtils.scala
+++ src/test/scala/au/org/healthdirect/nhsd/codingchallenge/SparkTestUtils.scala
@@ -3,7 +3,7 @@ package au.org.healthdirect.nhsd.codingchallenge
 import org.apache.spark.sql.SparkSession
 
 object SparkTestUtils {
-  def withSpark[T](f: SparkSession => T): Unit = {
+  def withSpark[T](f: SparkSession => T): T = {
     val session = new SparkSession.Builder()
       .appName("test")
       .config("spark.default.parallelism", 2)
